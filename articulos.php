fb1259bdf2153270b17e63d86e0b30cf1b205dec
gil-666 <143546119+gil-666@users.noreply.github.com>
2024-04-25 12:16:52 -0600

    A

--- articulos.php
+++ articulos.php
@@ -27,7 +27,7 @@
         }
     </style>
     <br>
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
         <?php
         // Seleccionar todos los artículos de la tabla 'articulos'
         $query = $conn->query("SELECT * FROM articulos");
