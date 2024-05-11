4184a8f5546ad818b5413e10a086693f05c4ba13
gil-666 <143546119+gil-666@users.noreply.github.com>
2024-05-10 18:23:29 -0600

    database al revesxd

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$server = 'sql.freedb.tech';
-$username = 'freedb_intel2024';
-$password = 'rBxUn5*%EESvNv!';
-$database = 'freedb_tiendaelectronica';
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'tiendaelectronica';
 
 try {
     $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
--- databaseremote.php
+++ databaseremote.php
@@ -1,8 +1,8 @@
 <?php
-$server = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'tiendaelectronica';
+$server = 'sql.freedb.tech';
+$username = 'freedb_intel2024';
+$password = 'rBxUn5*%EESvNv!';
+$database = 'freedb_tiendaelectronica';
 
 try {
     $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
